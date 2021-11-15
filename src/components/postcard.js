62e30fe6e3234893685b5a429ecd4cde36b43889
jenny han <jlhan@stanford.edu>
2021-11-15 01:13:55 -0800

    upload gatsby markdown template to avoid doing real work

---
+++ src/components/postcard.js
@@ -0,0 +1,22 @@
+/**
+ * Bio component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.com/docs/use-static-query/
+ */
+
+import * as React from "react"
+const styles = require("../postcard.css")
+
+const Postcard = () => {
+  return (
+    <div>
+      <div className="cardBox">
+        <div className="card">
+
+      </div>
+    </div>
+  )
+}
+
+export default Postcard
